3922250f91e19f30edd8a148281c6274411fd4cb
Wouter de Bie <wouter.de.bie@datadoghq.com>
2022-07-06 15:16:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# uhppote-sys
+
+This crate contains FFI bindings for [uhppoted-dll](https://github.com/uhppoted/uhppoted-dll)
+that's part of the [uhppoted](https://github.com/uhppoted/uhppoted) project.
+
+This crate provides low-level and unsafe access to `uhppoted-dll`. Please use ['uhppote-rs'](https://crates.io/crates/uhppote-rs) for safe Rust access to `uhppoted-dll`.
+
+**NOTE** Because `uhppoted-dll` is a Golang library, a working golang installation is necessary to build and use this crate.
+
+## How this is built
+
+Most of this crate is generated in `build.rs`.
+
+- The original `uhppoted-dll` source code can be found in `src/vendoruhppoted-dll` and is compiled using `go build` to compile a static archive.
+- The resulting `uhppoted-dll` static library is then copied to the `target/` directory.
+- `bindgen` is used to generate the Rust bindings from the `uhppoted-dll` C header file.
+- The Rust bindings and static library are then compiled and linked into a single library.
---
+++ build.rs
@@ -0,0 +1,56 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    println!("cargo:rustc-link-search=native={}", &out_path.display());
+    println!("cargo:rustc-link-lib=static=uhppoted");
+
+    let status = Command::new("go")
+        .args([
+            "build",
+            "-trimpath",
+            "-buildmode=c-archive",
+            "-o",
+            &out_path.join("libuhppoted.a").display().to_string(),
+            "go/devices.go",
+            "go/cards.go",
+            "go/events.go",
+            "go/time_profiles.go",
+            "go/tasks.go",
+            "go/main.go",
+        ])
+        .current_dir("vendor/uhppoted-dll")
+        .status()
+        .expect("Make upstream uhppoted-dll failed");
+
+    if !status.success() {
+        panic!("Make upstream uhppoted-dll failed");
+    }
+
+    let bindings = bindgen::Builder::default()
+        .header("vendor/uhppoted-dll/lib/libuhppoted.h")
+        .allowlist_function(
+            "^Get.*|^Set.*|^Open.*|^Put.*|^Delete.*|^Record.*|^Clear.*|^Refresh.*|^Add.*",
+        )
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        // .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    println!(
+        "cargo:rerun-if-changed={}/vendor/uhppoted-dll/lib/libuhppoted.h",
+        manifest_path.display()
+    );
+}
